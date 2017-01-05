310b17fb4c8c82730282ca968de6bdb9aec73fe8
markgreenburg <marek.greenburg@gmail.com>
2017-01-05 11:33:07 -0600

    migrate to heroku

---
+++ models.py
@@ -0,0 +1,288 @@
+"""
+CRUD & related models for our wiki app
+"""
+from time import strftime
+import pg
+import config
+
+class User(object):
+    """
+    User mapping for users in db
+    """
+    def __init__(self, user_id=0):
+        if not isinstance(user_id, int):
+            user_id = int(user_id)
+        sql = "SELECT id, username, password FROM users WHERE id=$1"
+        result_set = Database.get_result(sql, user_id)
+        if result_set:
+            self.user_id = user_id
+            self.username = result_set[0].username
+            self.password = result_set[0].password
+        else:
+            self.user_id = 0
+            self.username = "Not Set"
+            self.password = "Not Set"
+        return
+
+    @staticmethod
+    def get_user(username):
+        """
+        finds user by username and returns that user's object.
+        """
+        sql = "SELECT id FROM users WHERE username=$1"
+        user_id_list = Database.get_result(sql, username)
+        user_id = user_id_list[0].id
+        user = User(user_id)
+        return user
+
+class Page(object):
+    """
+    Object map for pages in the database
+    """
+    def __init__(self, page_id=0):
+        if not isinstance(page_id, int):
+            page_id = int(page_id)
+        sql = ("SELECT id, title, content, last_modified, modified_by,"
+               " deleted FROM page WHERE id=$1")
+        # Query our db to see if this page ID exists
+        result_set = Database.get_result(sql, page_id)
+        # If page exists, set Page attributes to what's in the db
+        if result_set:
+            self.page_id = page_id
+            self.title = result_set[0].title
+            self.content = result_set[0].content
+            self.last_modified = result_set[0].last_modified
+            self.modified_by = result_set[0].modified_by
+            self.deleted = result_set[0].deleted
+        else:
+            self.page_id = 0
+            self.title = "Not Set"
+            self.content = "Not Set"
+            self.last_modified = "Not Set"
+            self.modified_by = "Not Set"
+            self.deleted = False
+        return
+
+    def insert(self):
+        """
+        Inserts new page into wiki. Should not be called directly - instead,
+        use the Page.save() method.
+        """
+        self.last_modified = strftime("%Y-%m-%d %H:%M:%S")
+        sql = ("INSERT INTO page (title, content, last_modified, deleted,"
+               " modified_by) VALUES ($1, $2, $3, $4, $5)"
+               " RETURNING id")
+        result_list = Database.get_result(sql, \
+                      Database.escape(self.title), \
+                      Database.escape(self.content), \
+                      self.last_modified, \
+                      self.deleted, \
+                      Database.escape(self.modified_by)
+                                         )
+        self.page_id = result_list[0].id
+        print "PAGE ID IS: %s" % self.page_id
+        return self.page_id
+
+    def update(self):
+        """
+        Updates page in wiki. Should not be called directly - instead, use the
+        Page.save() method.
+        """
+        self.last_modified = strftime("%Y-%m-%d %H:%M:%S")
+        query = ("UPDATE page SET title = '%s', content = '%s', last_modified"
+                 " = '%s', modified_by = '%s' WHERE id = %d RETURNING id" % \
+                (Database.escape(self.title),\
+                 Database.escape(self.content),\
+                 self.last_modified,\
+                 Database.escape(self.modified_by),\
+                 self.page_id))
+        result_list = Database.get_result(query)
+        return result_list[0].id
+
+    def save(self):
+        """
+        Saves action to database by calling insert() or update() methods. If
+        page already exists, will call update(), else will call insert().
+        After insert or update, adds the last change to the revisions table.
+        """
+        if self.page_id:
+            self.update()
+        else:
+            self.insert()
+        revision = Revision()
+        revision.page_id = self.page_id
+        revision.title = self.title
+        revision.content = self.content
+        revision.last_modified = self.last_modified
+        revision.modified_by = self.modified_by
+        revision.deleted = self.deleted
+        revision.save()
+        return self.page_id
+
+    def set_delete(self, deleted=True):
+        """
+        Sets a page's 'deleted' attribute to 1 by default (so it won't show in
+        index) or 0 if passed in as an arg. Setting to 0 restores the page in
+        the index.
+        """
+        query = "UPDATE page SET deleted = %s WHERE id = %d RETURNING id" \
+                % (deleted, self.page_id)
+        return Database.get_result(query)
+
+    def get_revisions(self):
+        """
+        Fetches all undeleted pages from the database if no page_id entered. If
+        page_id arg given, returns all historical revisions of that page_id
+        from the revisions table. Returns all rows.
+        """
+        sql = ("SELECT id, page_id, title, content, last_modified,"
+               "modified_by FROM revisions WHERE page_id=$1 ORDER BY"
+               " last_modified DESC")
+        result_set = Database.get_result(sql, self.page_id)
+        revisions = []
+        for revision in result_set:
+            revision_id = int(revision[0])
+            revisions.append(Revision(revision_id))
+        return revisions
+
+    @staticmethod
+    def get_pages():
+        """
+        Fetches all undeleted pages from the database if no page_id entered. If
+        page_id arg given, returns all historical revisions of that page_id
+        from the revisions table. Returns all rows.
+        """
+        query = ("SELECT id, title, content, last_modified, modified_by"
+                 " FROM page WHERE deleted = FALSE ORDER BY title ASC")
+        result_set = Database.get_result(query)
+        pages = []
+        for page in result_set:
+            page_id = int(page[0])
+            pages.append(Page(page_id))
+        return pages
+
+    @staticmethod
+    def find_pages(search_string):
+        """
+        Searches db with wildcard search on title and content.
+        """
+        search_string = Database.escape(search_string)
+        sql = ("SELECT id FROM page WHERE title LIKE $1 OR content"
+               " LIKE $1")
+        search_string = '%' + search_string + '%'
+        result_set = Database.get_result(sql, search_string)
+        pages = []
+        for page in result_set:
+            page_id = int(page[0])
+            pages.append(Page(page_id))
+        return pages
+
+class Revision(object):
+    """
+    Subclass of Page. Over-rides some of the in-built functions to pull from
+    the revisions table by ID instead of from the page table
+    """
+    def __init__(self, revision_id=0):
+        if not isinstance(revision_id, int):
+            revision_id = int(revision_id)
+        sql = ("SELECT id, page_id, title, content, last_modified,"
+               " modified_by, deleted FROM revisions WHERE id=$1")
+        result_set = Database.get_result(sql, revision_id)
+        if result_set:
+            self.revision_id = revision_id
+            self.page_id = result_set[0].page_id
+            self.title = result_set[0].title
+            self.content = result_set[0].content
+            self.last_modified = result_set[0].last_modified
+            self.modified_by = result_set[0].modified_by
+            self.deleted = result_set[0].deleted
+        else:
+            self.revision_id = 0
+            self.page_id = 0
+            self.title = "not set"
+            self.content = "not set"
+            self.last_modified = "not set"
+            self.modified_by = "not set"
+            self.deleted = False
+
+    def insert(self):
+        """
+        inserts each change to the page table as a new entry into the revisions
+        table.
+        """
+        sql = ("INSERT INTO revisions (page_id, title, content,"
+               " last_modified, modified_by, deleted) VALUES ("
+               " $1, $2, $3, $4, $5, $6) RETURNING id")
+        result_list = Database.get_result(sql, \
+                      self.page_id, \
+                      Database.escape(self.title), \
+                      Database.escape(self.content), \
+                      self.last_modified, \
+                      Database.escape(self.modified_by), \
+                      self.deleted)
+        self.revision_id = result_list[0].id
+        return self.revision_id
+
+    def save(self):
+        """
+        Implemented for consistency with Page class methods. This method
+        simply calls the insert() method for the Revision class.
+        """
+        self.insert()
+        return self.revision_id
+
+class Database(object):
+    """
+    Collection of static methods that setup our DB connection and create
+    generalized methods for running queries / establish and release
+    connections in mysql
+    """
+    @staticmethod
+    def get_connection():
+        """
+        Sets up the postgreSQL connection. Uses settings from the config file.
+        """
+        return pg.DB(
+            host=config.host,
+            user=config.user_name,
+            passwd=config.password,
+            dbname=config.database
+            )
+
+    @staticmethod
+    def escape(value):
+        """
+        Escapes apostrophes in SQL
+        """
+        return value.replace("'", "''")
+
+    @staticmethod
+    def get_result(query, *args):
+        """
+        Opens a connection to the db, executes a query, fetches results, and
+        then closes the connection.
+        Args: query to execute, get_one (false by default, default is fetchall)
+        Returns: the fetchOne or fetchAll of the query
+        """
+        conx = Database.get_connection()
+        query = conx.query(query, *args)
+        result_list = query.namedresult()
+        conx.close()
+        return result_list
+
+    # @staticmethod
+    # def do_query(query):
+    #     """
+    #     Takes care of opening and then closing our database connections. Also
+    #     executes our SQL and auto-commits queries. Takes SQL query as arg and
+    #     returns the lastrowid in case we need to insert it as foreign key
+    #     elsewhere.
+    #     """
+    #     conx = Database.get_connection()
+    #     cur = conx.cursor()
+    #     cur.execute(query)
+    #     conx.commit()
+    #     last_id = cur.lastrowid
+    #     cur.close()
+    #     conx.close()
+    #     return last_id
---
+++ view.py
@@ -0,0 +1,218 @@
+"""
+Main routes and app configs file for wiki
+"""
+
+import sys
+from flask import Flask, Markup, render_template, request, redirect, flash, \
+session
+from flask_breadcrumbs import Breadcrumbs, register_breadcrumb
+import models
+from wiki_linkify import wiki_linkify
+import markdown
+
+#####
+# app configs: default app encoding, app name, specify config file, load Breadcrumbs import
+#####
+reload(sys)
+sys.setdefaultencoding('utf8')
+app = Flask("awesome_wiki")
+app.config.from_object('config')
+Breadcrumbs(app=app)
+
+#####
+# app routes
+#####
+@app.route("/")
+@register_breadcrumb(app, '.', 'Home')
+def homepage():
+    """
+    Directory of all pages in the wiki
+    """
+    page_list = models.Page.get_pages()
+    return render_template("homepage.html", page_list=page_list, \
+    title="Page Index")
+
+@app.route("/submit_search", methods=["GET"])
+@register_breadcrumb(app, '.search', 'Search Results')
+def submit_search():
+    """
+    Posts search form contents and redirects to a results page.
+    """
+    search_query = request.args.get("search_string")
+    print search_query
+    page_list = models.Page.find_pages(search_query)
+    return render_template("homepage.html", page_list=page_list, \
+    title="Search Results")
+
+@app.route("/login")
+@register_breadcrumb(app, '.login', 'Log In')
+def login():
+    """
+    Presents login form to user.
+    """
+    return render_template("login.html")
+
+@app.route("/submit_login", methods=["POST"])
+def submit_login():
+    """
+    submits login credentials to authenticate user, or redirects to login page.
+    """
+    entered_username = request.form.get('username')
+    entered_password = request.form.get('password')
+    user = models.User.get_user(entered_username)
+    if user.password == entered_password:
+        session['username'] = user.username
+        return redirect("/")
+    flash("Incorrect username or password")
+    return redirect("/login")
+
+@app.route("/logout")
+def logout():
+    """
+    Logs out current user by deleting their username from the session
+    """
+    del session['username']
+    return redirect("/")
+
+@app.route("/new_page")
+@register_breadcrumb(app, '.new_page', 'Add Page')
+def new_page():
+    """
+    Displays form to allow entry of data for new page creation.
+    """
+    if 'username' in session:
+        return render_template("new_page.html", page=models.Page())
+    flash("You must be logged in to add a page")
+    return redirect("/login")
+
+@app.route("/new_page_save", methods=["POST"])
+def insert_page():
+    """
+    Saves the new form content
+    """
+    page = models.Page()
+    page.title = request.form.get("title")
+    page.content = request.form.get("content")
+    page.modified_by = session['username']
+    page.save()
+    flash("Page '%s' created successfully" % page.title)
+    return redirect("/")
+
+@app.route("/view/<int:page_id>")
+@register_breadcrumb(app, '.view', 'View Page')
+def show_page(page_id):
+    """
+    Shows the contents of a specific page
+    """
+    page = models.Page(page_id)
+    page.content = wiki_linkify(page.content)
+    page.content = Markup(markdown.markdown(page.content))
+    return render_template("view.html", page_id=page.page_id, \
+                           title=page.title, \
+                           content=page.content,\
+                           last_modified=page.last_modified,\
+                           modified_by=page.modified_by)
+
+@app.route("/edit/<int:page_id>", methods=["GET"])
+@register_breadcrumb(app, '.view.edit', 'Edit Page')
+def edit_page(page_id):
+    """
+    Shows form to edit the content of the current page. Current values are
+    displayed in the form by default.
+    """
+    if 'username' in session:
+        page = models.Page(page_id)
+        return render_template("edit.html", page_id=page.page_id, \
+                               title=page.title, content=page.content,\
+                               modified_by=page.modified_by)
+    flash("You must be logged in to edit a page")
+    return redirect("/login")
+
+@app.route("/edit_page_save/<int:page_id>", methods=["POST"])
+def update_page(page_id):
+    """
+    Updates data for a given page id.
+    """
+    page = models.Page(page_id)
+    page.title = request.form.get("title")
+    page.content = request.form.get("content")
+    page.modified_by = session['username']
+    page.save()
+    flash("Page '%s' updated successfully" % page.title)
+    return redirect("/")
+
+@app.route("/delete/<int:page_id>")
+def delete_page(page_id):
+    """
+    Soft deletes a specific page. Flashes link to user to undo action
+    """
+    if 'username' in session:
+        page = models.Page(page_id)
+        page.set_delete()
+        flash("Page '%s' deleted successfully. <a href='/undelete/%d'>Undo</a>"\
+         % (page.title, page.page_id))
+        return redirect("/")
+    flash("You must be logged in to delete a page")
+    return redirect("/login")
+
+@app.route("/undelete/<int:page_id>")
+def undelete_page(page_id):
+    """
+    Sets 'deleted' attribute back to 0, effectively restoring the page.
+    """
+    page = models.Page(page_id)
+    page.set_delete(False)
+    flash("Page '%s' restored successfully." % page.title)
+    return redirect("/")
+
+@app.route("/history/<int:page_id>")
+@register_breadcrumb(app, '.view.history', 'Revision History')
+def show_history(page_id):
+    """
+    Shows historical versions of a page given the page id.
+    """
+    if 'username' in session:
+        current_version = models.Page(page_id)
+        page_list = current_version.get_revisions()
+        return render_template("history.html", page_list=page_list, \
+        title="Revision History: %s" % current_version.title)
+    flash("You must be logged in to view revision history")
+    return redirect("/login")
+
+@app.route("/rollback/<int:revision_id>")
+def rollback(revision_id):
+    """
+    Updates the page table with content from a given historical snapshot in the
+    revisions table. Takes the PK id in revisions table as arg
+    """
+    pull_from = models.Revision(revision_id)
+    overwrite_with = models.Page(pull_from.page_id)
+    overwrite_with.title = pull_from.title
+    overwrite_with.content = pull_from.content
+    overwrite_with.modified_by = pull_from.modified_by
+    overwrite_with.deleted = pull_from.deleted
+    overwrite_with.save()
+    flash("Page %s rolled back successfully." % overwrite_with.title)
+    return redirect("/")
+
+#####
+# Helper routes
+#####
+@app.route('/<file_name>.txt')
+def send_text_file(file_name):
+    """Send your static text file."""
+    file_dot_text = file_name + '.txt'
+    return app.send_static_file(file_dot_text)
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=600'
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
